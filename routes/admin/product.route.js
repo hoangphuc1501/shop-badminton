749bdfd5e6f3c3eacd1f1f0a3639c1ee9d0e95c5
hoangphuc1501 <katakurilancer.com>
2024-10-23 15:22:47 +0700

    validate dữ liệu

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const multer  = require('multer');
+//  lưu hình ảnh
 const storage = multer.diskStorage({
         destination: function (req, file, cb) {
         cb(null, './public/uploads/')
@@ -11,7 +12,8 @@ const storage = multer.diskStorage({
         }
     });
 const upload = multer({ storage: storage});
-
+// hết lưu hình ảnh
+const validate = require("../../validates/admin/product.validate");
 const controller = require("../../controllers/admin/product.controller");
 
 route.get("/", controller.index);
@@ -21,5 +23,9 @@ route.patch("/change-multi", controller.changeMulti);
 route.patch("/delete", controller.delete);
 route.patch("/change-position", controller.changePosition);
 route.get("/create", controller.create);
-route.post("/create", upload.single('thumbnail'),controller.createPost);
+route.post("/create", 
+    upload.single('thumbnail'),
+    validate.createPost,
+    controller.createPost
+);
 module.exports = route;
