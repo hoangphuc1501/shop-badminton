e91b41847245c0289a2535957824f909b6751eaf
hoangphuc1501 <katakurilancer.com>
2024-10-23 14:13:21 +0700

    Tạo slug

--- models/product.model.js
+++ models/product.model.js
@@ -1,7 +1,13 @@
 const mongoose  = require("mongoose");
-
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 const productSchema = new mongoose.Schema({
     title: String,
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
+    },
     description: String,
     price: Number,
     discountPercentage: Number,
