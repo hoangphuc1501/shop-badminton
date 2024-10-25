c8a184893c4d9ea740b082d89a118e999e8c77e3
hoangphuc1501 <katakurilancer.com>
2024-10-25 18:04:06 +0700

    Tạo trang danh sách tài khoản

---
+++ routes/admin/account.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const route = express.Router();
+const controller = require("../../controllers/admin/account.controller");
+
+route.get("/", controller.index);
+
+module.exports = route;
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const productCategory = require("./product-category.route");
 const roleRoute = require("./role.route");
+const accountRoute = require("./account.route");
 const trashRoute = require("./trash.route");
 
 const systemConfig = require("../../config/system");
@@ -12,6 +13,7 @@ module.exports = (app) => {
     app.use(`${PATH_ADMIN}/products`, productRoute);
     app.use(`${PATH_ADMIN}/products-category`, productCategory);
     app.use(`${PATH_ADMIN}/roles`, roleRoute);
+    app.use(`${PATH_ADMIN}/accounts`, accountRoute);
 
 
 
