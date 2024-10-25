ea0fa94b1c6b3e3d2751be1dead713680292e421
hoangphuc1501 <katakurilancer.com>
2024-10-25 17:10:51 +0700

    Danh sách nhóm quyền

---
+++ controllers/admin/role.controller.js
@@ -0,0 +1,11 @@
+const Role = require("../../models/role.model");
+
+module.exports.index = async (req, res) => {
+    const records = await Role.find({
+        deleted: false
+    });
+    res.render("admin/pages/roles/index.pug",{
+        pageTitle: "Nhóm quyền",
+        records: records
+    });
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const productCategory = require("./product-category.route");
+const roleRoute = require("./role.route");
 const trashRoute = require("./trash.route");
 
 const systemConfig = require("../../config/system");
@@ -10,6 +11,11 @@ module.exports = (app) => {
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
     app.use(`${PATH_ADMIN}/products`, productRoute);
     app.use(`${PATH_ADMIN}/products-category`, productCategory);
+    app.use(`${PATH_ADMIN}/roles`, roleRoute);
+
+
+
     app.use(`${PATH_ADMIN}/trash`, trashRoute);
+    
 }
 
