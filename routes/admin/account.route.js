89de16310d16ab9136d0037850bf117b71ad251d
hoangphuc1501 <katakurilancer.com>
2024-10-25 19:17:57 +0700

    Tạo tài khoản admin

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const route = express.Router();
+const multer  = require('multer');
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+const upload = multer();
+
+
+const uploadCloud = require("../../middlewares/admin/uploadcloud.middleware");
 const controller = require("../../controllers/admin/account.controller");
 
 route.get("/", controller.index);
-
+route.get("/create", controller.create);
+route.post("/create",
+    upload.single('avatar'),
+    uploadCloud.uploadSingle,
+    controller.createPost);
 module.exports = route;
