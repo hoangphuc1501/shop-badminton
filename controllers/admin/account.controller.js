051462383d989e8d72d840fd5b4d0c29e09626b8
hoangphuc1501 <katakurilancer.com>
2024-10-25 19:22:33 +0700

    Sửa trang danh sách tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -5,9 +5,21 @@ const md5 = require('md5');
 const generateHelper = require("../../helpers/generate.helper");
 const systemConfig = require("../../config/system");
 
-module.exports.index = (req, res) => {
-    res.render("admin/pages/accounts/index.pug",{
-        title: "Danh sách tài khoản"
+module.exports.index = async (req, res) => {
+    const records = await Account.find({
+        deleted: false
+    });
+    for (const item of records) {
+        const role = await Role.findOne({
+            _id: item.role_id,
+            deleted: false
+        });
+        item.role_title = role.title;
+    }
+
+    res.render("admin/pages/accounts/index.pug", {
+        title: "Danh sách tài khoản",
+        records: records
     });
 }
 
@@ -15,7 +27,7 @@ module.exports.create = async (req, res) => {
     const roles = await Role.find({
         deleted: false
     });
-    res.render("admin/pages/accounts/create.pug",{
+    res.render("admin/pages/accounts/create.pug", {
         title: "Tạo tài khoản",
         roles: roles
     });
