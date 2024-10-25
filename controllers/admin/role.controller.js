a1a7ce9eae7d06b34024e1ec13893c87872e78b7
hoangphuc1501 <katakurilancer.com>
2024-10-25 17:34:43 +0700

    Chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -11,9 +11,6 @@ module.exports.index = async (req, res) => {
     });
 }
 module.exports.create = async (req, res) => {
-    // const records = await Role.find({
-    //     deleted: false
-    // });
     res.render("admin/pages/roles/create.pug",{
         pageTitle: "Tạo nhóm quyền"
     });
@@ -23,4 +20,28 @@ module.exports.createPost = async (req, res) => {
     await role.save();
     req.flash("success", "Tạo nhóm quyền thành công");
     res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+}
+
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    const role = await Role.findOne({
+        _id: id,
+        deleted:false
+    })
+    res.render("admin/pages/roles/edit.pug",{
+        pageTitle: "Chỉnh sửa nhóm quyền",
+        role: role
+    });
+}
+
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    const role = await Role.updateOne({
+        _id: id,
+        deleted:false
+    }, req.body)
+    req.flash("success", "Cập nhật thành công!");
+    res.redirect("back");
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -5,5 +5,6 @@ const controller = require("../../controllers/admin/role.controller");
 route.get("/", controller.index);
 route.get("/create", controller.create);
 route.post("/create", controller.createPost);
-
+route.get("/edit/:id", controller.edit);
+route.patch("/edit/:id", controller.editPatch);
 module.exports = route;
