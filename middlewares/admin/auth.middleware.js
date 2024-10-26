195cffe9c4c8ffbf210e007f5453268791957e7b
hoangphuc1501 <katakurilancer.com>
2024-10-26 16:38:28 +0700

    Phân quyền sơ lược

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,8 +1,8 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
-    console.log(req.cookies.token);
     if(!req.cookies.token){
         res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
         return;
@@ -17,5 +17,13 @@ module.exports.requireAuth = async (req, res, next) => {
         res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
         return;
     }
+
+    const role = await Role.findOne({
+        _id: user.role_id,
+        deleted: false
+    })
+    
+    res.locals.user = user
+    res.locals.role = role   
     next();
 }
