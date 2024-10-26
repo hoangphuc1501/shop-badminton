1f86183886d3d05014fa9f47d817f792747727de
hoangphuc1501 <katakurilancer.com>
2024-10-27 02:45:26 +0700

    danh sách danh muc sản phẩm bên Client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const productsRoute = require("./product.route");
 const homeRoute = require("./home.route");
 
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 module.exports = (app) =>{
+    app.use(categoryMiddleware.category);
     app.use("/", homeRoute);
 
     app.use("/products", productsRoute)
