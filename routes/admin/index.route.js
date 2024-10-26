96acba05f6c030ff349e21d7fc5f8707fcb43294
hoangphuc1501 <katakurilancer.com>
2024-10-26 14:45:58 +0700

    Thêm middleware cho route private

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -7,18 +7,37 @@ const authRoute = require("./auth.route");
 const trashRoute = require("./trash.route");
 
 const systemConfig = require("../../config/system");
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
 module.exports = (app) => {
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
 
-    app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
-    app.use(`${PATH_ADMIN}/products`, productRoute);
-    app.use(`${PATH_ADMIN}/products-category`, productCategory);
-    app.use(`${PATH_ADMIN}/roles`, roleRoute);
-    app.use(`${PATH_ADMIN}/accounts`, accountRoute);
+    app.use(`${PATH_ADMIN}/dashboard`,
+        authMiddleware.requireAuth,
+        dashboardRoute
+    );
+    app.use(`${PATH_ADMIN}/products`, 
+        authMiddleware.requireAuth,
+        productRoute
+    );
+    app.use(`${PATH_ADMIN}/products-category`,
+        authMiddleware.requireAuth,
+        productCategory
+    );
+    app.use(`${PATH_ADMIN}/roles`, 
+        authMiddleware.requireAuth,
+        roleRoute
+    );
+    app.use(`${PATH_ADMIN}/accounts`,
+        authMiddleware.requireAuth, 
+        accountRoute
+    );
     app.use(`${PATH_ADMIN}/auth`, authRoute);
 
 
-    app.use(`${PATH_ADMIN}/trash`, trashRoute);
+    app.use(`${PATH_ADMIN}/trash`, 
+        authMiddleware.requireAuth,
+        trashRoute
+    );
     
 }
 
