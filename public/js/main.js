3bc758c1efef8fa2081302a79f1feeed9209fd0d
hoangphuc1501 <katakurilancer.com>
2024-10-27 19:04:27 +0700

    Cập nhật số lượng sản phẩm trong giỏ hàng

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,72 @@
+// table-cart
+const tableCart = document.querySelector("[table-cart]");
+console.log(tableCart)
+if (tableCart) {
+    const listInputQuantity = tableCart.querySelectorAll("input[name='quantity']");
+    listInputQuantity.forEach(input => {
+        input.addEventListener("change", () => {
+            const productId = input.getAttribute("item-id");
+            const quantity = input.value;
+            fetch("/cart/update", {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "PATCH",
+                body: JSON.stringify({
+                    productId: productId,
+                    quantity: quantity
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.code == "success") {
+                        location.reload();
+                    }
+                })
+        })
+    })
+}
+// End table-cart
+
+
 // list hình ảnh
 var imgFeature = document.querySelector('.img-feature')
-    var listImg = document.querySelectorAll('.list-image img')
-    var prevBtn = document.querySelector('.prev')
-    var nextBtn = document.querySelector('.next')
-    var currentindex = 0;
-    function updateImageByIndex(index) {
-        document.querySelectorAll('.list-image div').forEach(item => {
-            item.classList.remove('active')
-        })
-        currentindex = index
-        imgFeature.src = listImg[index].getAttribute('src')
-        listImg[index].parentElement.classList.add('active')
+var listImg = document.querySelectorAll('.list-image img')
+var prevBtn = document.querySelector('.prev')
+var nextBtn = document.querySelector('.next')
+var currentindex = 0;
+function updateImageByIndex(index) {
+    document.querySelectorAll('.list-image div').forEach(item => {
+        item.classList.remove('active')
+    })
+    currentindex = index
+    imgFeature.src = listImg[index].getAttribute('src')
+    listImg[index].parentElement.classList.add('active')
+}
+listImg.forEach((imgElement, index) => {
+    imgElement.addEventListener('click', e => {
+        updateImageByIndex(index)
+    })
+})
+
+prevBtn.addEventListener('click', e => {
+    if (currentindex == 0) {
+        currentindex = listImg.length - 1
+    } else {
+        currentindex--
     }
-    listImg.forEach((imgElement, index) => {
-        imgElement.addEventListener('click', e => {
-            updateImageByIndex(index)
-        })
-    })
+    updateImageByIndex(currentindex)
+})
+nextBtn.addEventListener('click', e => {
+    if (currentindex == listImg.length - 1) {
+        currentindex = 0
+    } else {
+        currentindex++
+    }
+    updateImageByIndex(currentindex)
+})
 
-    prevBtn.addEventListener('click', e => {
-        if (currentindex == 0) {
-            currentindex = listImg.length - 1
-        } else {
-            currentindex--
-        }
-        updateImageByIndex(currentindex)
-    })
-    nextBtn.addEventListener('click', e => {
-        if (currentindex == listImg.length - 1) {
-            currentindex = 0
-        } else {
-            currentindex++
-        }
-        updateImageByIndex(currentindex)
-    })
-
-    updateImageByIndex(0)
+updateImageByIndex(0)
 // hết list hình ảnh
 
 // category list
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -6,4 +6,5 @@ const controller = require("../../controllers/client/cart.controller");
 route.get("/", controller.index);
 route.post("/add/:id", controller.addPost);
 route.get("/delete/:id", controller.delete);
+route.patch("/update", controller.updatePatch);
 module.exports = route;
