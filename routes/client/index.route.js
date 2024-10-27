8863eadc0785ad78e29b8e2d2fd50f0111bb0575
hoangphuc1501 <katakurilancer.com>
2024-10-27 16:45:00 +0700

    Thêm mới giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,11 +1,14 @@
 const productsRoute = require("./product.route");
 const homeRoute = require("./home.route");
+const cartRoute = require("./cart.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 module.exports = (app) =>{
     app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cart );
     app.use("/", homeRoute);
-
     app.use("/products", productsRoute)
+    app.use("/cart", cartRoute)
 };
