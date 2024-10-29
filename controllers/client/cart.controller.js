f100dd40284424023c0367832eba97ffab4e617b
hoangphuc1501 <katakurilancer.com>
2024-10-29 23:50:04 +0700

    sửa cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -7,7 +7,8 @@ module.exports.index = async (req, res) => {
     const cart = await Cart.findOne({
         _id: cartId
     });
-    const products = cart.products;
+    // const products = cart.products;
+    const products = cart ? cart.products : []; // Nếu cart là null thì products sẽ là một mảng rỗng
     let total = 0;
     for (const item of products) {
         const infoItem = await Product.findOne({
